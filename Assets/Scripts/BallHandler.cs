4fe38a8a2cabd6b6195dc2e91d38cb8f57808eb4
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-08-07 06:41:14 -0500

    Added touch controls

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -5,17 +5,29 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
+    [SerializeField] Rigidbody2D currentBallRigidbody;
+
+    private Camera mainCamera;
+
     void Start()
     {
-        
+        mainCamera = Camera.main;
     }
 
     void Update()
     {
-        if (!Touchscreen.current.primaryTouch.press.isPressed) { return; }
+        if (!Touchscreen.current.primaryTouch.press.isPressed) 
+        {
+            currentBallRigidbody.isKinematic = false;
+            return; 
+        }
+
+        currentBallRigidbody.isKinematic = true;
 
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
-        Debug.Log(touchPosition);
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+
+        currentBallRigidbody.position = worldPosition;
     }
 }
